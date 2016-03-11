c2fa8f0ad90395e6358772f81207207c5627d3a6
Nikolay Akopov <asoman97@gmail.com>
2016-03-12 00:24:35 +0300

    2 урок и начало 3. FPSInput, MouseLook, RayShoot(начало)

---
+++ Unity Learning Project/Assets/Scripts/2ndLesson/MouseLook.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour
+{
+
+
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitiveHor = 3.0f;
+    public float sensitiveVert = 3.0f;
+
+    public float minVert = -45.0f;
+    public float maxVert = 45.0f;
+
+    private float _rotationX = 0;
+    private float _rotationY = 0;
+
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    void Update()
+    {
+        switch (axes)
+        {
+            case RotationAxes.MouseXAndY:
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitiveVert;
+                _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+                float delta = Input.GetAxis("Mouse X") * sensitiveHor;
+                _rotationY = transform.localEulerAngles.y + delta;
+
+                transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+                break;
+            case RotationAxes.MouseX:
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitiveHor, 0);
+                break;
+            case RotationAxes.MouseY:
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitiveVert;
+                _rotationX = Mathf.Clamp(_rotationX, minVert, maxVert);
+
+                float rotationY = transform.localEulerAngles.y;
+
+                transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+                break;
+        }
+
+    }
+}
---
+++ Unity Learning Project/Assets/Scripts/3dLesson/RayShooter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+    private Camera _cam;
+
+	// Use this for initialization
+	void Start () {
+        _cam = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(_cam.pixelWidth / 2, _cam.pixelHeight / 2, 0);
+            Ray ray = _cam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                StartCoroutine(SphereIndicator(hit.point));
+        }
+	}
+
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
