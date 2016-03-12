735416c096644b7dd59ed69b792ee194e19996b7
Nikolay Akopov <asoman97@gmail.com>
2016-03-13 01:33:46 +0300

    3rd lesson. shooting and react-to-shoot-target

--- Unity Learning Project/Assets/Scripts/3dLesson/RayShooter.cs
+++ Unity Learning Project/Assets/Scripts/3dLesson/RayShooter.cs
@@ -20,7 +20,16 @@ public class RayShooter : MonoBehaviour {
             Ray ray = _cam.ScreenPointToRay(point);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
-                StartCoroutine(SphereIndicator(hit.point));
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+
+                if (target != null)
+                    target.ReactToHit();
+                else
+                    StartCoroutine(SphereIndicator(hit.point));
+
+            }
         }
 	}
 
